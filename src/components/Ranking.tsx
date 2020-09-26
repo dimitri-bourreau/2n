4f8dbf962de80d78f91d8a0e82aa5cb28cb620c0
tobudim <tobudim@protonmail.com>
2020-09-26 15:45:46 +0200

    update ranking with map on array, later to be fetched from server

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,45 +1,35 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, { FunctionComponent, ReactElement, useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { Trans } from 'react-i18next';
-import { Alert } from 'shards-react';
+import { Alert, Badge } from 'shards-react';
 
 const Ranking: FunctionComponent<RouteComponentProps> = (): ReactElement => {
+  // temp ranking var, leter TODO server to store
+  const [ranking] = useState([
+    { name: 'SORA', score: 32 },
+    { name: 'TOBU', score: 10 },
+  ]);
+
   return (
     <div id="ranking">
       <h1 className="my-5 mx-auto text-center">
         <Trans i18nKey="ranking.title">Ranking</Trans>
       </h1>
 
-      <Alert theme="warning" block square className="text-center">
-        1 - TOBU - 3345
-      </Alert>
-      <Alert theme="success" block square className="text-center">
-        2 - ZFEZ - 234
-      </Alert>
-      <Alert theme="success" block square className="text-center">
-        3 - FDVS - 221
-      </Alert>
-      <Alert theme="info" block square className="text-center">
-        4 - GUFF - 193
-      </Alert>
-      <Alert theme="info" block square className="text-center">
-        5 - FFSE - 154
-      </Alert>
-      <Alert theme="info" block square className="text-center">
-        6 - HTRH - 123
-      </Alert>
-      <Alert theme="light" block square className="text-center">
-        7 - FEZH - 112
-      </Alert>
-      <Alert theme="light" block square className="text-center">
-        8 - JIYG - 111
-      </Alert>
-      <Alert theme="light" block square className="text-center">
-        9 - YUYV - 111
-      </Alert>
-      <Alert theme="light" block square className="text-center">
-        10 - TYHD - 110
-      </Alert>
+      {ranking.map((user, index) => (
+        <Alert
+          key={`${user.name}${user.score}`}
+          theme="light"
+          className="mb-1"
+          block
+          square
+        >
+          <Badge theme="info" className="mr-2" pill>
+            {index + 1}
+          </Badge>{' '}
+          {user.name} - {user.score}
+        </Alert>
+      ))}
     </div>
   );
 };
