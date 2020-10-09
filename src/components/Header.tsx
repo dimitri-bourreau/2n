d9fa6f3094ae286d33503a4d5f547d6814cdd877
tobudim <tobudim@protonmail.com>
2020-10-09 21:32:26 +0200

    work in progress : update header composition and style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,67 @@
 import React, { FunctionComponent, ReactElement } from 'react';
-import { Trans } from 'react-i18next';
-import { Nav, NavItem, NavLink } from 'shards-react';
-import LangDropDown from './LangDropDown';
+import style from 'styled-components';
+// import { Trans } from 'react-i18next';
+// import { Nav, NavItem, NavLink } from 'shards-react';
+// import LangDropDown from './LangDropDown';
 
-const Header: FunctionComponent = (): ReactElement => {
-  const activePath = window.location.pathname.split('').splice(1).join('');
+const HeaderWrapper = style.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = style.div`
+  flex-basis: 10%;
+  img {
+    max-width: 100%;
+  }
+`;
+
+const List = style.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ListElt = style.li`
+  a {
+    color: black;
+  }
+`;
+
+interface HeaderProps {
+  userIsConnected?: boolean;
+}
+
+const Header: FunctionComponent<HeaderProps> = ({
+  userIsConnected = false,
+}: HeaderProps): ReactElement => {
+  // const activePath = window.location.pathname.split('').splice(1).join('');
 
   return (
-    <div id="header">
-      <Nav tabs>
-        <NavItem>
-          <NavLink active={activePath === ''} href="/">
-            <Trans i18nKey="header.game">Game</Trans>
-          </NavLink>
-        </NavItem>
-        <NavItem href="more">
-          <NavLink active={activePath === 'more'} href="more">
-            <Trans i18nKey="header.more">More</Trans>
-          </NavLink>
-        </NavItem>
-
-        <div className="ml-auto mr-0 mt-1">
-          <LangDropDown />
-        </div>
-      </Nav>
-    </div>
+    <HeaderWrapper>
+      <Logo>
+        <img src="/favicons/android-chrome-512x512.png" alt="logo" />
+      </Logo>
+      <nav>
+        <List>
+          <ListElt>
+            <a href="/">Accueil</a>
+          </ListElt>
+          <ListElt>
+            <a href="/classements">Classements</a>
+          </ListElt>
+          <ListElt>
+            <a href="/apropos">À propos</a>
+          </ListElt>
+          <ListElt>
+            <a href={userIsConnected ? '/me' : '/login'}>
+              {userIsConnected ? 'Mon compte' : 'Se connecter'}
+            </a>
+          </ListElt>
+        </List>
+      </nav>
+    </HeaderWrapper>
   );
 };
 
