a3bae0c584d30c09dac8359838129127685f28d5
tobudim <tobudim@protonmail.com>
2020-10-11 16:50:33 +0200

    define animation

---
+++ public/2-digits-canvas/animate-canvas.mjs
@@ -0,0 +1,10 @@
+/**
+ * Start animation for all twos in canvas
+ * @param twos Array
+ * @param context Object
+ * @param animation Array
+ * -> Void
+ */
+export default function animateCanvas(twos, context, animation) {
+  console.log('animating...', animation);
+}
--- public/2-digits-canvas/main.mjs
+++ public/2-digits-canvas/main.mjs
@@ -1,3 +1,5 @@
+import animateCanvas from './animate-canvas.mjs';
+import defineAnimation from './define-animation.mjs';
 import defineHowManyColumns from './define-how-many-columns.mjs';
 import defineHowManyLines from './define-how-many-lines.mjs';
 import fixDimensions from './fix-dimensions.mjs';
@@ -7,17 +9,14 @@ import writeAndReturnTwos from './write-and-return-twos.mjs';
   const canvas = document.getElementById('2-digits-canvas');
   if (!canvas.getContext) return; // stop if no support
 
-  const twos = [];
+  const { width, height } = fixDimensions(canvas);
+  const context = canvas.getContext('2d');
+  const columns = defineHowManyColumns(width);
+  const lines = defineHowManyLines(height);
+  const twos = writeAndReturnTwos(columns, lines, context);
+  const animation = defineAnimation(lines);
 
   window.requestAnimationFrame(() => {
-    const { width, height } = fixDimensions(canvas);
-    const context = canvas.getContext('2d');
-    const columns = defineHowManyColumns(width);
-    const lines = defineHowManyLines(height);
-    if (!twos.length) {
-      twos = writeAndReturnTwos(columns, lines, context);
-    } else {
-      console.log('ok');
-    }
+    animateCanvas(twos, context, animation);
   });
 })();
