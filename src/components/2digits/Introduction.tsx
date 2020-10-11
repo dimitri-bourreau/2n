3f3b8dba65a4c2eb467646b8987f390143c92c32
tobudim <tobudim@protonmail.com>
2020-10-12 00:31:32 +0200

    update introduction responsiveness and style

--- src/components/2digits/Introduction.tsx
+++ src/components/2digits/Introduction.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactElement, useEffect } from 'react';
+import React, { FunctionComponent, ReactElement } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import style from 'styled-components';
 
@@ -6,13 +6,9 @@ import IntroductionCanvas from './IntroductionCanvas';
 
 // import { Trans } from 'react-i18next';
 
-interface IntroductionProps extends RouteComponentProps {
-  handlePlayGame?: (game: boolean) => null;
-}
-
 const IntroductionWrapper = style.div`
   width: 100%;
-  height: 95vh;
+  height: 390px;
   background: linear-gradient(221deg, #2b3ba7, #b38737, #3eaba5, #bc6f55);
   background-size: 800% 800%;
   position: relative;
@@ -40,30 +36,50 @@ const IntroductionWrapper = style.div`
 
 const IntroductionCard = style.section`
   width: 80%;
-  min-height: 200px;
   margin: auto;
   background-color: rgba(255,255,255,1); 
   position: absolute;
-  top: 30%;
+  top: 80px;
   left: 0;
   right: 0;
+  text-align: center;
+  padding: 20px;
+  box-sizing: border-box;
+
+  @media all and (min-width: 576px) {
+    width: 60%;
+  }
+  @media all and (min-width: 768px) {
+    width: 40%;
+  }
+  @media all and (min-width: 992px) {
+    width: 30%;
+  }
+  @media all and (min-width: 1200px) {
+    width: 30%;
+  }
 `;
 
-const Introduction: FunctionComponent<IntroductionProps> = ({
-  handlePlayGame,
-}: IntroductionProps): ReactElement => {
+const Introduction: FunctionComponent<RouteComponentProps> = (): ReactElement => {
   return (
     <IntroductionWrapper>
       <IntroductionCanvas />
       <IntroductionCard>
-        <h1>Bienvenue sur 2n !</h1>
+        <h1>2n</h1>
+        <h2>
+          Souvenez-vous il y a deux chiffres.{' '}
+          <span role="img" aria-label="think">
+            🤔
+          </span>
+        </h2>
+        <a href="/">
+          <button className="btn btn-play" type="button">
+            Jouer
+          </button>
+        </a>
       </IntroductionCard>
     </IntroductionWrapper>
   );
-};
-
-Introduction.defaultProps = {
-  handlePlayGame: () => null,
 };
 
 export default Introduction;
