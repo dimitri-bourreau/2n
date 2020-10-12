a58a15e11f3f1f962d3b32c8b3b8d865527c02a1
tobudim <tobudim@protonmail.com>
2020-10-12 22:29:47 +0200

    update gameover display

--- src/components/2digits/GameOver.tsx
+++ src/components/2digits/GameOver.tsx
@@ -1,20 +1,51 @@
 import React, { FunctionComponent, ReactElement } from 'react';
+import style from 'styled-components';
 
 import { EndOfGameProps } from '../../interfaces/game';
+
+const GameOverWrapper = style.div`
+  margin: 30px auto;
+  width: 80%;
+  text-align: center;
+  min-height: 80vh;
+  padding: 30px;
+  box-sizing: border-box;
+
+  h1 {
+    margin-bottom: 50px;
+  }
+
+  button {
+    margin-top: 50px;
+  }
+
+  @media all and (min-width: 576px) {
+    width: 70%;
+  }
+  @media all and (min-width: 768px) {
+    width: 60%;
+  }
+  @media all and (min-width: 992px) {
+    width: 50%;
+  }
+  @media all and (min-width: 1200px) {
+    width: 30%;
+  }
+`;
 
 const GameOver: FunctionComponent<EndOfGameProps> = ({
   score,
 }: EndOfGameProps): ReactElement => {
   return (
-    <div id="end-of-game" className="text-center mb-4">
-      <div className="mt-5 text-light">
-        <h1 className="text-light">Game over!</h1>
-        <h3 className="text-light">Your score</h3>
-      </div>
-      <a href="/">
-        <div className="mt-4">Play again</div>
+    <GameOverWrapper>
+      <h1>Game over!</h1>
+      <h2>Your score : {score}</h2>
+      <a href="/game">
+        <button type="button" className="btn btn-success">
+          Play again
+        </button>
       </a>
-    </div>
+    </GameOverWrapper>
   );
 };
 
