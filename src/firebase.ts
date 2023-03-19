e4c08a5e805716d8094e7f5c4f644f098fc2818f
Dimitri Bourreau <contact@dimitribourreau.me>
2023-03-19 17:30:55 +0100

    remove firebase config file

--- src/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyAZciIBN9EI3Gsiv543AkXUELgQDx-2PEM',
-  authDomain: 'n-game-7d0a4.firebaseapp.com',
-  databaseURL: 'https://n-game-7d0a4.firebaseio.com',
-  projectId: 'n-game-7d0a4',
-  storageBucket: 'n-game-7d0a4.appspot.com',
-  messagingSenderId: '239293493619',
-  appId: '1:239293493619:web:6574233aec06ad6492d506',
-};
